36d466cc15f8d9b8e11e41f2c11ec41ac97f9785
58050360 <46705467+58050360@users.noreply.github.com>
2019-01-23 21:52:51 +0700

    Sign Up Front&BackEnd Connect

--- app/src/main/java/com/skyzhaneul/herelkb/SignUpActivity.java
+++ app/src/main/java/com/skyzhaneul/herelkb/SignUpActivity.java
@@ -1,7 +1,6 @@
 package com.skyzhaneul.herelkb;
 
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,11 +14,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
-    ProgressBar progressBar;
-    EditText txt_signemail, txt_signpassword;
+    private ProgressBar progressBar;
+    private  EditText txt_signemail, txt_signpassword, txt_signname, txt_signphone;
 
     private FirebaseAuth mAuth;
 
@@ -29,48 +29,91 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_sign_up);
         txt_signemail = (EditText) findViewById(R.id.txt_signemail);
         txt_signpassword = (EditText) findViewById(R.id.txt_signpassword);
+        txt_signname = (EditText) findViewById(R.id.txt_signname);
+        txt_signphone = (EditText) findViewById(R.id.txt_signphone);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        progressBar.setVisibility(View.GONE);
 
         mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.gosignup_action).setOnClickListener(this);
         findViewById(R.id.gologin_action).setOnClickListener(this);
 
     }
-    private void registerUser(){
-        String email = txt_signemail.getText().toString().trim();
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth.getCurrentUser() != null) {
+
+        }
+    }
+
+    private void registerUser() {
+        final String email = txt_signemail.getText().toString().trim();
         String password = txt_signpassword.getText().toString().trim();
-        if(email.isEmpty()){
+        final String name = txt_signname.getText().toString().trim();
+        final String phone = txt_signphone.getText().toString().trim();
+        if (email.isEmpty()) {
             txt_signemail.setError("Email is required");
             txt_signemail.requestFocus();
             return;
         }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             txt_signemail.setError("Please enter a valid email");
             txt_signemail.requestFocus();
             return;
         }
-        if(password.isEmpty()){
+        if (password.isEmpty()) {
             txt_signpassword.setError("Password is required");
             txt_signpassword.requestFocus();
             return;
         }
-        if(password.length()<6){
+        if (password.length() < 6) {
             txt_signpassword.setError("Minimum lengt of password should be 6");
             txt_signpassword.requestFocus();
             return;
         }
+        if (phone.isEmpty()) {
+            txt_signphone.setError("Phone is required");
+            txt_signphone.requestFocus();
+            return;
+        }
+        if (phone.length() != 10) {
+            txt_signphone.setError("Enter a valid phone number");
+            txt_signphone.requestFocus();
+            return;
+        }
         progressBar.setVisibility(View.VISIBLE);
-       mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-           @Override
-           public void onComplete(@NonNull Task<AuthResult> task) {
-               progressBar.setVisibility(View.GONE);
-               if(task.isSuccessful()) {
-                   Toast.makeText(getApplicationContext(),"User Register Successful",Toast.LENGTH_SHORT).show();
-               } else { Toast.makeText(getApplicationContext(),"Some Error occured",Toast.LENGTH_SHORT).show();
-               }
-           }
-       });
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    User user = new User(
+                            name,
+                            email,
+                            phone
+                    );
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "User Register Successful", Toast.LENGTH_SHORT).show();
+                            } else {
+                                //display a failure message
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(getApplicationContext(), "Some Error occured", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
---
+++ app/src/main/java/com/skyzhaneul/herelkb/User.java
@@ -0,0 +1,14 @@
+package com.skyzhaneul.herelkb;
+
+public class User {
+    public String name, email ,phone ;
+
+    public  User() {
+
+    }
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
